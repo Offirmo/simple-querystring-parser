a7df72b5955d9a25a04de559235e58e10307756b
Offirmo <offirmo.net@gmail.com>
2017-03-29 18:51:57 +1100

    switch to ES5 for PhantomJS

--- README.md
+++ README.md
@@ -5,10 +5,11 @@ This version departs from the venerable https://github.com/sindresorhus/query-st
 - only 75 lines, no dependencies
 - parses only (lighter)
 - attempts to do a light type inference from the values (number, boolean)
-- interprets "no value" as true (boolean) **XXX this is non-standard** but we consider it better (i.e. interpreting it as a flag)
+- interprets "no value" as true (boolean) **XXX this is [non-standard](http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters)** but we consider it better (i.e. interpreting it as a flag)
 - typescript compatible
 - UMD compatible, but also exposes itself as a global var, even when loaded by a module loader. Reason: this lib may be needed by low-level code, for ex. a "verbose" option activating logs should be detected very early, maybe from inline code without a loader.
 
+Tested in latest Chrome, FF and Safari and PhantomJS versions.
 
 # Usage
 
--- index.js
+++ index.js
@@ -6,16 +6,16 @@
 	if (typeof define === 'function' && define.amd) {
 		// AMD. Register as an anonymous module.
 		define(function () {
-			return (root.SimpleQuerystringParser = factory());
+			return (root.SimpleQuerystringParser = factory())
 		});
 	} else if (typeof module === 'object' && module.exports) {
 		// Node. Does not work with strict CommonJS, but
 		// only CommonJS-like environments that support module.exports,
 		// like Node.
-		module.exports = factory();
+		module.exports = factory()
 	} else {
 		// Browser globals
-		root.SimpleQuerystringParser = factory();
+		root.SimpleQuerystringParser = factory()
 	}
 }(this, function () {
 	"use strict";
@@ -39,11 +39,12 @@
 		return val // no inference
 	}
 
-	function parse(querystring, options = {}) {
+	function parse(querystring, options) {
+		options = options || {}
 		options.valueDecoder = options.valueDecoder || lightQuerystringValueDecoder
 		// Create an object with no prototype
 		// https://github.com/sindresorhus/query-string/issues/47
-		const result = Object.create(null)
+		var result = Object.create(null)
 
 		if (typeof querystring !== 'string')
 			return result
@@ -54,23 +55,25 @@
 		if (!querystring)
 			return result
 
-		const raw_options = querystring.split('&')
-		return raw_options.reduce((acc, raw_option) => {
+		var raw_options = querystring.split('&')
+		return raw_options.reduce(function (acc, raw_option) {
 			if (!raw_option) return acc
 
-			const [ key, value ] = raw_option.split('=').map(decodeURIComponent)
+			var split = raw_option.split('=')
+			var key = decodeURIComponent(split[0])
+			var value = decodeURIComponent(split.slice(1).join('=')) || undefined
 			acc[key] = options.valueDecoder(value)
 			return acc
 		}, result)
 	}
 
-	function parseLocationParams(location, options = {}) {
+	function parseLocationParams(location, options) {
 		return parse(location.search, options)
 	}
 
 	return {
-		parse,
-		parseLocationParams,
-		lightQuerystringValueDecoder,
+		parse: parse,
+		parseLocationParams: parseLocationParams,
+		lightQuerystringValueDecoder: lightQuerystringValueDecoder
 	}
 }))
